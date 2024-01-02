3d9b7296ed7f714ebb88db1f02480f94fdb394bb
Japserrr <dev.jaspergraber@gmail.com>
2024-01-02 20:57:44 +0100

    Fix e-mail test result tables

--- BusinessLogic/Services/EmailService.cs
+++ BusinessLogic/Services/EmailService.cs
@@ -4,6 +4,7 @@ using BusinessLogic.Guards;
 using BusinessLogic.Interfaces.Services;
 using BusinessLogic.IRepositories;
 using BusinessLogic.Models;
+using System.Text;
 
 namespace BusinessLogic.Services;
 
@@ -46,19 +47,20 @@ public class EmailService
         return $@"
             <html>
                 <body>
-                    <h1 style='text-align:center'>Resultaten gehoortest</h1>
-                    <h2 style='text-align:center;color:#DA0063'>{result}</h2>
+                    <h1 style='text-align:center;'>Resultaten gehoortest</h1>
+                    <h2 style='text-align:center;color:#DA0063;'>{result}</h2>
                     <br>
                     <span>U heeft een gehoortest bij ons gedaan op:</span>
-                    <span style='display:block'>{date}</span>
+                    <span style='display:block;'>{date}</span>
                     <br>
-                    <span style='display:block'>De resultaten duiden op <span style='color:#DA0063'>{result.ToLower()}</span>.</span>
+                    <span style='display:block;'>De resultaten duiden op <span style='color:#DA0063;'>{result.ToLower()}</span>.</span>
                     <br>
-                    <span style='display:block'>U gaf aan de getestte frequenties op onderstaand decibel niveau te horen:</span>
+                    <span style='display:block;'>U gaf aan de getestte frequenties op onderstaand decibel niveau te horen:</span>
                     <br>
-                    <h3 style='margin-bottom: 0;'>Rechter Oor</h3>
+                    <h3 style='margin-bottom:0;color:#DA0063;'>Rechter Oor</h3>
                     {rightEarTable}
-                    <h3 style='margin-bottom: 0;'>Linker Oor</h3>
+                    <br>
+                    <h3 style='margin-bottom:0;color:#DA0063;'>Linker Oor</h3>
                     {leftEarTable}
                     <br>
                     <p>De audicien kan u advies geven op basis van de testresultaten. maak een afspraak met de audicien.</p>
@@ -69,110 +71,80 @@ public class EmailService
 
     private string CreateResultTable(List<ToneAudiometryQuestionResult> answers)
     {
-        string tableString = $@"
-            <p style='margin: 0;'>HL (dB)</p> 
-            <table style='border-top: 1px solid; border-right: 1px solid; border-spacing: 0; border-collapse: collapse;'>
-        ";
-        
-        // Create table rows for answers (decibel range 0 - 120)
-        for (int row = 1; row <= 27; row++)
+        StringBuilder stringBuilder = new StringBuilder();
+
+        // Open the table HTML
+        stringBuilder.Append(@"
+            <p style='margin:0;'>HL (dB)</p> 
+            <table style='border-top:1px solid;border-right:1px solid;border-spacing:0;border-collapse:collapse;'>
+        ");
+
+        // Create the result rows for each decibel
+        for (int row = 0; row <= 26; row++)
         {
-            int currentDecibels = (row - 2) * 5;
+            stringBuilder.Append("<tr>");
 
-            tableString += "<tr>";
-            for (int column = 1; column <= 13; column++)
+            int currentDecibels = (row - 1) * 5;
+            if (currentDecibels % 2 == 0) 
             {
-                switch (column)
-                {
-                    // Add decibels or empty column
-                    case 1:
-                        tableString += (row % 2 == 0)
-                            ? ColumnBuilder(content: currentDecibels.ToString())
-                            : ColumnBuilder();
-                        continue;
-
-                    // Add empty column
-                    case 2:
-                        tableString += ColumnBuilder(border: true);
-                        continue;
-
-                    // Add column in frequency range (column 3 to 13)
-                    default:
-                        tableString += CreateFrequencyRangeColumn(column, currentDecibels, answers);
-                        continue;
-                }
+                stringBuilder.Append($"<td style='height:20px;text-align:right;'>{currentDecibels}</td>");
+            } 
+            else {
+                stringBuilder.Append($"<td style='height:20px'></td>");
             }
-            tableString += "</tr>";
+
+            // Create columns with the result for each frequency
+            CreateResultColumns(currentDecibels, answers, stringBuilder);
+
+            stringBuilder.Append("</tr>");
         }
 
-        // Create last table row with all frequencies
-        tableString += CreateFrequencyRow();
-        
-        tableString += @"
+        // Create the last row with the frequencies
+        CreateLastFrequencyRow(stringBuilder);
+
+        // Close the table HTML
+        stringBuilder.Append(@"
             </table>
-            <p style='margin: 0;'>Frequentie (Hz)</p> 
-        ";
+            <p style='margin:0;'>Frequentie (Hz)</p> 
+        ");
 
-        return tableString;
+        return stringBuilder.ToString();
     }
 
-    private string CreateFrequencyRangeColumn(int column, int currentDecibels, List<ToneAudiometryQuestionResult> answers)
+    private void CreateResultColumns(int currentDecibels, List<ToneAudiometryQuestionResult> answers, StringBuilder stringBuilder)
     {
-        FrequencyMap frequencyMap = FrequencyMapping.Frequencies[column - 3];
-
-        ToneAudiometryQuestionResult? answer = answers.FirstOrDefault(r => r.Frequency == frequencyMap.Frequency);
-        if (answer == null) return ColumnBuilder(border: true);
-
-        string content = "";
-        if (answer.LowestDecibels == currentDecibels) content = "X";
-
-        // Determine whether the answer is in hearing loss range
-        int min = frequencyMap.HearingLoss.Min;
-        int max = frequencyMap.HearingLoss.Max;
-
-        if (min == 0 && max == 0) return ColumnBuilder(content: content, border: true);
-
-        return (min <= answer.LowestDecibels && max >= answer.LowestDecibels)
-            ? ColumnBuilder(content: content, border: true, background: true)
-            : ColumnBuilder(content: content, border: true);
-    }
-
-    private string CreateFrequencyRow()
-    {
-        string rowString = "<tr>";
-        for (int column = 1; column <= 13; column++)
+        for (int column = 0; column <= 8; column++)
         {
-            switch (column)
+            FrequencyMap currentFrequency = FrequencyMapping.Frequencies[column];
+            ToneAudiometryQuestionResult? answer = answers.FirstOrDefault(item => item.Frequency == currentFrequency.Frequency);
+
+            int min = currentFrequency.HearingLoss.Min;
+            int max = currentFrequency.HearingLoss.Max;
+
+            string background = "";
+            if (min <= currentDecibels && max >= currentDecibels)
             {
-                // Add empty column
-                case 1: 
-                case 2:
-                    rowString += ColumnBuilder();
-                    continue;
-
-                // Add column with frequency
-                default:
-                    FrequencyMap frequencyMap = FrequencyMapping.Frequencies[column - 3];
-                    rowString += $"<td style='text-align: center; width: 25px'>{frequencyMap.FrequencyString}</td>";
-                    continue;
+                background = "background-color:red;";
             }
-        }
-        rowString += "</tr>";
 
-        return rowString;
+            if (answer == null || currentDecibels != answer.LowestDecibels)
+            {
+                stringBuilder.Append($"<td style='border:1px solid;{background}'></td>");
+                continue;
+            }
+
+            stringBuilder.Append($"<td style='border:1px solid;text-align:center;font-weight:bold;{background}'>x</td>");
+        }
     }
 
-    private string ColumnBuilder(string? content = null, bool border = false, bool background = false)
+    private void CreateLastFrequencyRow(StringBuilder stringBuilder)
     {
-        if (content != null && border && background) return $"<td style='font-weight: bold; border: 1px solid; background-color: red; text-align: center;'>{content}</td>";
-
-        if (content != null && border) return $"<td style='font-weight: bold; border: 1px solid; text-align: center;'>{content}</td>";
-
-        if (content != null) return $"<td style='text-align: end;'>{content}</td>";
-
-        if (border) return $"<td style='border: 1px solid; height: 10px; width: 10px;'></td>";
-      
-        // Empty filler column
-        return $"<td style='height: 10px; width: 10px;'></td>";
+        stringBuilder.Append("<tr>");
+        stringBuilder.Append("<td></td>");
+        foreach (FrequencyMap frequencyMap in FrequencyMapping.Frequencies)
+        {
+            stringBuilder.Append($"<td style='width:35px;text-align:center;'>{frequencyMap.FrequencyString}</td>");
+        }
+        stringBuilder.Append("<tr>");
     }
 }
