baac6bc20312f19e7dcec38cfbf031c36627eac4
Dinny van Huizen <dinnyvanh@hotmail.com>
2024-01-12 19:44:13 +0100

    removed unncessary code and added auto generating of password when creating new employee

--- UserInterface/ViewModels/Modals/ErrorModalViewModal.cs
+++ UserInterface/ViewModels/Modals/ErrorModalViewModal.cs
@@ -6,23 +6,16 @@ namespace UserInterface.ViewModels.Modals;
 
 internal class ErrorModalViewModal : ViewModelBase
 {
-    #region Dependencies
     private readonly NavigationStore navigationStore;
-    #endregion
 
-    #region Properties
     private string _text;
     public string Text
     {
         get { return _text; }
         set { _text = value; OnPropertyChanged(nameof(Text)); }
     }
-    #endregion
 
-    #region Commands
     public ICommand ConfirmCommand => new Command(Confirm);
-    public ICommand DenyCommand => new Command(Deny);
-    #endregion
 
     public ErrorModalViewModal(NavigationStore navigationStore, string text)
     {
@@ -33,10 +26,6 @@ internal class ErrorModalViewModal : ViewModelBase
     {
         CloseModal();
     }
-    private void Deny()
-    {
-        CloseModal();
-    }   
     private void CloseModal()
     {
         navigationStore.CloseModal();
